Fixed issue of pieces not being able to reach the edge of the board due to limitations of rotation

main.py
@@ -1,7 +1,3 @@
-import os
-import random
-import types
-
 import numpy as np
 from pyboy import PyBoy, WindowEvent
 import pygad.gann
@@ -53,15 +49,30 @@ def drop_down():
 # go n_dir spaces in direction, and rotate n_turn times
 def action(translate, turn):
     # if translate is negative, move left. Else, right
+    flag = 0
+    if translate <= -5:
+        flag = -1
+    elif translate >= 5:
+        flag = 1
 
     for _ in range(abs(translate)):
         if translate < 0:
             action_translate("Left")
+            action_translate("Left")
         else:
+            action_translate("Right")
             action_translate("Right")
 
     for _ in range(turn):
         action_turn()
+
+    if flag == -1:
+        action_translate("Left")
+        action_translate("Left")
+
+    elif flag == 1:
+        action_translate("Right")
+        action_translate("Right")
 
     drop_down()
 
@@ -178,6 +189,7 @@ def getCurrentBoard():
 # 17
 def get_predicted_board(translate, turn, tetromino=getCurrentTetromino(), startingBoard=getCurrentBoard()):
     num = -1
+    flag = 0
 
     coords = getStartCoords(tetromino)
 
@@ -190,6 +202,11 @@ def get_predicted_board(translate, turn, tetromino=getCurrentTetromino(), starti
         if board[2][column] != 0:
             # print("Prediction failure.")
             return False
+
+    if translate >= 5:
+        flag = 1
+    elif translate <= -5:
+        flag = -1
 
     while translate != 0:
         if coords[0][1] + translate < 0:
@@ -213,6 +230,26 @@ def get_predicted_board(translate, turn, tetromino=getCurrentTetromino(), starti
             else:
                 coords = tempCoords
                 turn = 0
+
+    move_to_edge = True
+    if flag == -1:
+        for row in range(0, len(coords)):
+            if coords[row][1] - 1 <= 0:
+                move_to_edge = False
+
+        if move_to_edge:
+            for row in range(0, len(coords)):
+                coords[row][1] -= 1
+                # coords[i][0] +=  1
+    elif flag == 1:
+        for row in range(0, len(coords)):
+            if coords[row][1] + 1 >= 10:
+                move_to_edge = False
+
+        if move_to_edge:
+            for row in range(0, len(coords)):
+                coords[row][1] += 1
+                # coords[i][0] +=  1
 
     drop = True
     while drop:
@@ -337,9 +374,9 @@ def calculateBestMove(w):
             # prediction = calculateReward(predictedBoard, weights[0], weights[1], weights[2], weights[3],
             # weights[4])
             prediction = w[0] * getCompleteLines(tempBoard) + \
-                         w[1] * getHoles(tempBoard) + \
-                         w[2] * getAggregateHeight(tempBoard) + \
-                         w[3] * getBumpiness(tempBoard)
+                w[1] * getHoles(tempBoard) + \
+                w[2] * getAggregateHeight(tempBoard) + \
+                w[3] * getBumpiness(tempBoard)
 
             for i2 in range(-6, 6):
                 for j2 in range(5):
@@ -353,9 +390,9 @@ def calculateBestMove(w):
                     #                               weights[2], weights[3], weights[4])
 
                     prediction2 = w[0] * getCompleteLines(predictedBoard2) + \
-                                  w[1] * getHoles(predictedBoard2) + \
-                                  w[2] * getAggregateHeight(predictedBoard2) + \
-                                  w[3] * getBumpiness(predictedBoard2)
+                        w[1] * getHoles(predictedBoard2) + \
+                        w[2] * getAggregateHeight(predictedBoard2) + \
+                        w[3] * getBumpiness(predictedBoard2)
 
                     if prediction + prediction2 > temp:
                         # print("temp check")
@@ -373,17 +410,13 @@ currentGen = 0
 
 
 # fitness function to be maximized
-def fitness_function(solution, solution_idx):
+def fitness_function(sol, sol_idx):
     global GANN_instance, ga_instance, currentGen
 
     # if ga_instance.generations_completed != currentGen:
     #     currentGen = ga_instance.generations_completed
     #     print("Generation = {generation}".format(generation=ga_instance.generations_completed), currentGen)
     #     ga_instance.save(filename='genetic')
-
-
-
-    print("::: ", len(solution), solution_idx, "\n")
 
     # board = getCurrentBoard()
 
@@ -392,13 +425,13 @@ def fitness_function(solution, solution_idx):
     pyboy.set_emulation_speed(0)
 
     score = 0
-    while solution_idx is not None:
+    while sol_idx is not None:
         board = getCurrentBoard()
 
         data_inputs = np.array([[getHoles(board), getBumpiness(board), getAggregateHeight(board),
                                  pyboy.get_memory_value(0xc203), pyboy.get_memory_value(0xC213)]])
         # print(solution_idx, data_inputs)
-        predictions = pygad.nn.predict(last_layer=GANN_instance.population_networks[solution_idx],
+        predictions = pygad.nn.predict(last_layer=GANN_instance.population_networks[sol_idx],
                                        data_inputs=data_inputs,
                                        problem_type="regression")
         # print("predictions =  ", predictions[0])
@@ -411,11 +444,11 @@ def fitness_function(solution, solution_idx):
         score += calculateReward(predictedBoard)
         pyboy.tick()
 
-    print(solution_idx, score, getLinesCleared())
+    print(sol_idx, score, getLinesCleared())
     return score
 
 
-GANN_instance = pygad.gann.GANN(num_solutions=5,
+GANN_instance = pygad.gann.GANN(num_solutions=250,
                                 num_neurons_input=5,
                                 num_neurons_hidden_layers=[5],
                                 num_neurons_output=5,
@@ -424,7 +457,7 @@ GANN_instance = pygad.gann.GANN(num_solutions=5,
 population_vectors = pygad.gann.population_as_vectors(population_networks=GANN_instance.population_networks)
 print(" . ", population_vectors)
 
-num_generations = 5
+num_generations = 50
 num_parents_mating = 2  # percentage of total population (sol_per_pop * 0.1)
 
 initial_population = population_vectors.copy()
@@ -438,9 +471,8 @@ print("\n ", len(population_vectors), len(initial_population), len(population_ve
 parent_selection_type = "tournament"
 keep_parents = 1
 crossover_type = "single_point"
-mutation_type = "adaptive"  # "adaptive" is IMPROVEMENT TO YL
+mutation_type = "random"  # "adaptive" is IMPROVEMENT TO YL
 mutation_percent_genes = np.array([50, 10])
-
 
 filename = 'genetic'
 
@@ -458,25 +490,22 @@ def on_gen(ga):
     ga_instance.save(filename=filename)
 
 
-def callback_generation(ga_instance):
-    global GANN_instance, last_fitness
-
-    population_matrices = pygad.gann.population_as_matrices(population_networks=GANN_instance.population_networks,
-                                                            population_vectors=ga_instance.population)
-
-    GANN_instance.update_population_trained_weights(population_trained_weights=population_matrices)
-
-    print("Generation = {generation}".format(generation=ga_instance.generations_completed))
-    print("Fitness    = {fitness}".format(fitness=ga_instance.best_solution()[1]))
-    print("Change     = {change}".format(change=ga_instance.best_solution()[1] - last_fitness))
-
-    last_fitness = ga_instance.best_solution()[1].copy()
-    last_fitness.save(filename=filename)
-
-    return "stop"
-
-
-
+# def callback_generation(ga_instance):
+#     global GANN_instance, last_fitness
+#
+#     population_matrices = pygad.gann.population_as_matrices(population_networks=GANN_instance.population_networks,
+#                                                             population_vectors=ga_instance.population)
+#
+#     GANN_instance.update_population_trained_weights(population_trained_weights=population_matrices)
+#
+#     print("Generation = {generation}".format(generation=ga_instance.generations_completed))
+#     print("Fitness    = {fitness}".format(fitness=ga_instance.best_solution()[1]))
+#     print("Change     = {change}".format(change=ga_instance.best_solution()[1] - last_fitness))
+#
+#     last_fitness = ga_instance.best_solution()[1].copy()
+#     last_fitness.save(filename=filename)
+#
+#     return "stop"
 
 
 ga_instance = pygad.GA(num_generations=num_generations,
@@ -486,7 +515,6 @@ ga_instance = pygad.GA(num_generations=num_generations,
                        parent_selection_type=parent_selection_type,
                        keep_parents=keep_parents,
                        crossover_type=crossover_type,
-                       mutation_percent_genes=mutation_percent_genes,
                        mutation_type=mutation_type,
                        on_generation=on_gen)
 